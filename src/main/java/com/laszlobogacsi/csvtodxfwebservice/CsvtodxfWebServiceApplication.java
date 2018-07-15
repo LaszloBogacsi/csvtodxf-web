ba73db0eef554255ecf13ac4344a7afa3cfd4ed8
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-07-15 23:00:19 +0100

    Initial commit

---
+++ src/main/java/com/laszlobogacsi/csvtodxfwebservice/CsvtodxfWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.laszlobogacsi.csvtodxfwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvtodxfWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvtodxfWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/laszlobogacsi/csvtodxfwebservice/CsvtodxfWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laszlobogacsi.csvtodxfwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsvtodxfWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
