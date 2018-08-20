2725d0b1fa5bc2e66cfab9e1567f61639f11a127
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-08-20 23:39:31 +0100

    Prepared support for filecompression for downloading

---
+++ src/main/java/com/laszlobogacsi/csvtodxfwebservice/file/GZipFileCompressorService.java
@@ -0,0 +1,10 @@
+package com.laszlobogacsi.csvtodxfwebservice.file;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class GZipFileCompressorService implements FileCompressorService {
+    @Override
+    public void compress(String pathToFileToCompress, String newFilePath) {
+        throw new NotImplementedException();
+    }
+}
