d3143810ac4cdd756092da142fc2a631131dbfd7
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-08-21 08:46:15 +0100

    Add gzip file compression service

--- src/main/java/com/laszlobogacsi/csvtodxfwebservice/file/GZipFileCompressorService.java
+++ src/main/java/com/laszlobogacsi/csvtodxfwebservice/file/GZipFileCompressorService.java
@@ -1,10 +1,24 @@
 package com.laszlobogacsi.csvtodxfwebservice.file;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.GZIPOutputStream;
 
 public class GZipFileCompressorService implements FileCompressorService {
     @Override
     public void compress(String pathToFileToCompress, String newFilePath) {
-        throw new NotImplementedException();
+        try(FileOutputStream outputStream = new FileOutputStream(newFilePath);
+            GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outputStream)) {
+
+            byte[] data = Files.readAllBytes(Paths.get(pathToFileToCompress));
+            gzipOutputStream.write(data);
+        } catch (FileNotFoundException e) {
+            // TODO; log a level up and handle exception
+        } catch (IOException ioe) {
+            // TODO; log a level up and handle exception
+        }
     }
 }
