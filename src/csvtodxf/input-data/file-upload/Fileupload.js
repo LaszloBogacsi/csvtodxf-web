5a67db97d7a933ab6258eb80349f0173512c7dc0
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-09-02 22:54:43 +0100

    Drag and drop zone

--- src/csvtodxf/input-data/file-upload/Fileupload.js
+++ src/csvtodxf/input-data/file-upload/Fileupload.js
@@ -1,26 +1,29 @@
 import React, {Component} from 'react'
 import * as Rx from "rxjs-compat";
+import Dropzone from "react-dropzone";
 
 class Fileupload extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            value: '',
+            files: [],
+            dropzoneActive: false
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.onDrop = this.onDrop.bind(this);
     }
 
     handleChange(event) {
-        const fileName = event.target.files[0].name;
-        this.setState({value: fileName});
-        this.handleSubmit(event, fileName);
+        const file = event.target.files[0];
+        this.setState({value: file.name});
+        this.handleSubmit(file);
     }
 
-    handleSubmit(e, fileName) {
-        this.props.onFileNameChange(fileName);
-        const file = e.target.files[0];
+    handleSubmit(file) {
+        this.props.onFileNameChange(file.name);
         let data = new FormData();
         data.append("file", file);
         const url = 'http://localhost:9090/upload-file';
@@ -39,11 +42,37 @@ class Fileupload extends Component {
             )
     }
 
+    onDragEnter() {
+        this.setState({
+            dropzoneActive: true
+        });
+    }
+
+    onDragLeave() {
+        this.setState({
+            dropzoneActive: false
+        });
+    }
+
+    onDrop(files, rejected) {
+        this.setState({
+            files: files,
+            dropzoneActive: false
+        });
+        if (rejected.length > 0) {
+            console.log("this filetype is not supported: " + rejected[0].name);
+        }
+        const oneFile = files[0];
+        this.handleSubmit(oneFile);
+
+    }
     render() {
         return (
             <form>
                 <label>Select a csv file form your computer</label>
                 <input type="file" name="upload" accept=".csv, .txt"  onChange={this.handleChange}/>
+                <Dropzone accept={"application/vnd.ms-excel, text/plain" } onDrop={this.onDrop}/>
+                <ul>{this.state.files.map((f, index) => <li key={index}>{f.name} - {f.size} bytes</li>)}</ul>
             </form>
         );
 
