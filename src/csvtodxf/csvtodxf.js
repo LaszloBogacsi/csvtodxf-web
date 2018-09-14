d8220f9ac333b9687509625892a4b32f352df00e
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-09-14 01:01:18 +0100

    Added progress header and wired up to change state accordingly

--- src/csvtodxf/csvtodxf.js
+++ src/csvtodxf/csvtodxf.js
@@ -1,26 +1,67 @@
 import React, {Component} from 'react'
 import InputData from "./input-data/InputData";
 import Result from "./results/Result";
-import {Container, Grid, Step} from "semantic-ui-react";
+import {Container, Grid} from "semantic-ui-react";
+import ProgressHeader from "./progress-header/ProgressHeader";
 
 
 class CsvToDxf extends Component {
     constructor(props) {
         super(props);
         this.handleConvertResponse = this.handleConvertResponse.bind(this);
-
+        this.progressAhead = this.progressAhead.bind(this);
+        this.finishStep = this.finishStep.bind(this);
+        this.handleProgress = this.handleProgress.bind(this);
         this.state = {
             convertResponse: '',
-            isConvertDone: false
+            isConvertDone: false,
+            step1IsDone: false,
+            step1Active: true,
+            step1Disabled: false,
+            step2IsDone: false,
+            step2Active: false,
+            step2Disabled: true,
+            step3IsDone: false,
+            step3Active: false,
+            step3Disabled: true
         }
     }
 
     handleConvertResponse(response) {
         this.setState({
             isConvertDone: true,
-            convertResponse: response
-        })
+            convertResponse: response,
+
+        });
+        this.handleProgress(3);
     }
+
+    handleProgress(stepNumber) {
+        console.log('called');
+        const MAX_NUMBER_OF_STEPS = 3;
+        if (stepNumber <= MAX_NUMBER_OF_STEPS) {
+            this.finishStep(stepNumber - 1);
+            this.progressAhead(stepNumber);
+        }
+    };
+
+
+
+    progressAhead(stepNumber) {
+        this.setState({
+            [`step${stepNumber}Active`]: true,
+            [`step${stepNumber}IsDone`]: false,
+            [`step${stepNumber}Disabled`]: false,
+        })
+    };
+
+    finishStep(stepNumber) {
+        this.setState({
+            [`step${stepNumber}Active`]: false,
+            [`step${stepNumber}IsDone`]: true,
+            [`step${stepNumber}Disabled`]: false,
+        })
+    };
 
     render() {
         const convertResponse = this.state.convertResponse;
@@ -30,19 +71,21 @@ class CsvToDxf extends Component {
         if(isConvertComplete) {
             result = <Result convertResponse={convertResponse}/>
         }
+        let step1 = {active : this.state.step1Active, isDone: this.state.step1IsDone, disabled: this.state.step1Disabled};
+        let step2 = {active: this.state.step2Active, isDone: this.state.step2IsDone, disabled: this.state.step2Disabled};
+        let step3 = {active: this.state.step3Active, isDone: this.state.step3IsDone, disabled: this.state.step3Disabled};
 
         return (
-            <Container textAlign='left' style={{width : 900}}>
+            <Container textAlign='left' style={{width : 700}}>
                 <h2>CSV to DXF converter</h2>
                 <h4>Convert csv survey data to Cad drawing</h4>
                 <Grid>
                     <Grid.Row>
-                            <Grid.Column width={8} >
-                                <InputData onConvertResponse={this.handleConvertResponse}/>
-                            </Grid.Column>
-                            <Grid.Column width={8} >
-                                {result}
-                            </Grid.Column>
+                        <ProgressHeader stepOne={step1} stepTwo={step2} stepThree={step3}/>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <InputData onProgress={this.handleProgress} onConvertResponse={this.handleConvertResponse}/>
+                        {result}
                     </Grid.Row>
                 </Grid>
             </Container>
--- src/csvtodxf/input-data/InputData.js
+++ src/csvtodxf/input-data/InputData.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import Fileupload from "./file-upload/Fileupload";
 import DrawingConfig from "./drawing-config/DrawingConfig";
-import {Icon, Step} from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
 
 class InputData extends Component {
     constructor(props) {
@@ -32,44 +32,25 @@ class InputData extends Component {
 
     handleUploadFinished(isDone) {
         this.setState({uploadDone: isDone});
+        this.props.onProgress(2);
     }
+
     render() {
         let fileName = this.state.fileName;
         let drawingId = this.state.drawingId;
         let isUploadComplete = this.state.uploadDone;
         let drawingConfig;
 
-        // if (isUploadComplete) {
-        if (true) {
+        if (isUploadComplete) {
+        // if (true) {
             drawingConfig =  <DrawingConfig fileName={fileName} drawingId={drawingId} onConvertResponse={this.handleConvertResponse}/>
         }
 
         return (
-            <div>
-                <Step.Group fluid>
-                    <Step completed={isUploadComplete} active={!isUploadComplete}>
-                        <Icon name="upload"/>
-                        <Step.Content>
-                            <Step.Title>{isUploadComplete ? 'Upload finished' : 'Upload'}</Step.Title>
-                            <Step.Description>{isUploadComplete ? fileName : 'Upload your csv file '}</Step.Description>
-                        </Step.Content>
-                    </Step>
-
-                </Step.Group>
-
+            <Container fluid>
                 <Fileupload onUploadFinished={this.handleUploadFinished} onFileNameChange={this.handleFileNameChange} onDrawingIdChange={this.handleDrawingIdChange}/>
-
-                <Step.Group fluid>
-                <Step completed={false} active={false}>
-                    <Icon name="options"/>
-                    <Step.Content>
-                        <Step.Title>Options</Step.Title>
-                        <Step.Description>Choose drawing options</Step.Description>
-                    </Step.Content>
-                </Step>
-                </Step.Group>
                 {drawingConfig}
-            </div>
+            </Container>
         );
 
     }
