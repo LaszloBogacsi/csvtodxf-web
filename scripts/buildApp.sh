77197400274c864ef5d7e1e10e9779ed34ae4961
Laszlo Bogacsi <laszlo.bogacsi@itv.com>
2019-02-07 19:24:20 +0000

    modify build script

--- scripts/buildApp.sh
+++ scripts/buildApp.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# build frontend
-cd ../csvtodxf-ui/
-npm run-script build
-
 # build backend
 cd ../
-mvn clean package -Pprod
+mvn clean package -P prod
 
 cp ./csvtodxf-service/target/csvtodxf-service-0.0.1-SNAPSHOT.jar .
